bec7993eddb8bf3629863c07d13da21b0c2db886
Ke Chen <CK@QWERT-2.local>
2015-11-02 21:07:42 -0600

    a

--- 01 Data/1.R
+++ 01 Data/1.R
@@ -2,7 +2,7 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 
-
+setwd("/Users/CK/DV_TProject1/01 Data")
 file_path <- "2016 FE Guide for DOE.csv"
 
 df <- read.csv(file_path, stringsAsFactors = FALSE)
@@ -34,7 +34,7 @@ names(df)[49] <-"five_years_savings"
 names(df)[50] <-"five_years_spend"
 names(df)[53] <-"Comb_CO2_Rounded_Adjusted"
 df <-select(df,1:53)
-str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+#str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
 
 measures <- c("Model_Year","Eng_Displ","Numbers_of_Cyl","City_FE_Guide","Hwy_FE_Guide","Comb_FE_Guide","Numbers_of_Gear","Max_Ethanol_Gasoline","Annual_Fuel_Cost","Calculated_Annual_Fuel_Cost","EPA_FE_Label_Dataset_ID","Numbers_of_Batteries","Total_Voltage_for_Battery","Batt_Energy_Cpacity_Amp_hrs","Batt_Specific_Energy_Watt","Numbers_of_Drive_Motor_Gen","Rated_Motor_Gen_Power_kW","Calculated_Gas_Guzzler_MPG","FE_Rating_1_10","five_years_savings","five_years_spend","City_CO2_Rounded_Adjusted","Hwy_CO2_Rounded_Adjusted","Comb_CO2_Rounded_Adjusted")
 #measures <- NA # Do this if there are no measures.
@@ -58,10 +58,6 @@ if( length(measures) > 1 || ! is.na(dimensions)) {
 }
 
 library(lubridate)
-# Fix date columns, this needs to be done by hand because | needs to be correct.
-#                                                        \_/
-df$Order_Date <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Order_Date), tz="UTC")))
-df$Ship_Date  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Ship_Date),  tz="UTC")))
 
 # The following is an example of dealing with special cases like making state abbreviations be all upper case.
 # df["State"] <- data.frame(lapply(df["State"], toupper))
